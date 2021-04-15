a083ed0419ecc3474cad3544169122e60948e686
Pavle Mijatovic <paul.mijatovic@sprintingsoftware.com>
2021-04-15 14:58:51 +0200

    Initial 2

---
+++ kiwiTest/App/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  kiwiTest
+//
+//  Created by Pavle Mijatovic on 15.4.21..
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
